de38295c1ef35e6f72e5453ee172dc104d46991b
yokra9 <53964890+yokra9@users.noreply.github.com>
2020-06-20 22:10:59 +0900

    スレをaxiosで取得

--- js/main.ts
+++ js/main.ts
@@ -1,12 +1,16 @@
-import("../pkg/index.js").then(wasm => {
-    const json = wasm.parse(`
-    
+import axios from "axios"
+const http = axios.create({
+    baseURL: "http://" + location.host
+})
 
-    
-    `)
+http.get("/sample2.html").then((res) => {
 
-    console.log(JSON.parse(json))
-}).catch(console.error);
+    import("../pkg/index.js").then(wasm => {
+        const json = wasm.parse(res.data)
+        console.log(JSON.parse(json))
+    }).catch(console.error);
+
+})
 
 import { createApp } from 'vue'
 import App from './App.vue'
