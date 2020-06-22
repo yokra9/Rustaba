28e30f77c707934319da42ed82c8891440846871
yokra9 <53964890+yokra9@users.noreply.github.com>
2020-06-22 20:37:27 +0900

    dockerへのデプロイスクリプトを追加、スマホで確認しやすく

--- js/App.vue
+++ js/App.vue
@@ -1,29 +1,56 @@
 <template>
-  <pre>{{ tmp }}</pre>
+  <input type="number" v-model="num" />
+  <select v-model="baseURL" @click="clearThread">
+    <option disabled value>板</option>
+    <option value="http://192.168.1.2:8081/mock-img">img</option>
+    <option value="http://192.168.1.2:8081/mock-may">may</option>
+  </select>
+  <input type="button" value="表示" @click="getThread" />
+  <div v-for="(c,i) in contributions" :key="c.quote">
+    <span>{{i}} {{c.title}} {{c.name}} {{c.date}}</span>
+    <div v-html="c.quote" />
+    <img v-if="c.image!==''" :src="baseURL + c.image" />
+  </div>
 </template>
 
 <script lang="ts">
-import { ref, defineComponent } from "vue";
+import { ref, computed, defineComponent } from "vue";
 import axios from "axios";
 
 export default defineComponent({
   setup() {
-    const http = axios.create({
-      baseURL: "http://" + location.host
-    });
+    const baseURL = ref("http://192.168.1.2:8081/mock-img");
 
-    const tmp = ref<String>();
-    http.get("/sample3.html").then(res => {
-      import("../pkg/index.js")
-        .then(wasm => {
-          const json = wasm.parse(res.data);
-          tmp.value = JSON.parse(json);
-        })
-        .catch(console.error);
-    });
+    const num = ref<Number>();
+    const contributions = ref();
+
+    const getThread = () => {
+      axios
+        .get(["b", "res", num.value + ".htm"].join("/"), { baseURL: baseURL.value })
+        .then(res => {
+          import("../pkg/index.js")
+            .then(wasm => {
+              const json = wasm.parse(res.data);
+              contributions.value = Object.assign(
+                {},
+                JSON.parse(json).contributions,
+                contributions.value
+              );
+            })
+            .catch(console.error);
+        });
+    };
+
+    const clearThread = () => {
+      contributions.value = {};
+    };
 
     return {
-      tmp
+      baseURL,
+      num,
+      contributions,
+      getThread,
+      clearThread
     };
   }
 });
